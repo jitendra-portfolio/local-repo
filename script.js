825650a31b4e973cb9f0d5e1f040bf4010d364d3
jitendra-portfolio <work.jitendrarathore@gmail.com>
2024-03-13 19:25:06 +0530

    second index added

---
+++ script.js
@@ -0,0 +1,14 @@
+ let score= prompt("enter the marks");
+
+ if(score>=90&&score<=100){
+   console.log("A")
+ }else if(score>=70&&score<90){
+   console.log("B")
+ }else if(score>=60&&score<70){
+   console.log("C")
+ }else if(score>=50&&score<60){
+   console.log("D")
+ }else{
+   console.log("fail")
+}
+
